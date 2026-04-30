6f52ff860ddf932900d109a65fd37da388662845
gtora <gtora85@gmail.com>
2026-04-30 21:12:03 +0500

    update 6

--- jastar_hub_backend/src/auth/dto/auth.dto.ts
+++ jastar_hub_backend/src/auth/dto/auth.dto.ts
@@ -1,10 +1,25 @@
+import { IsEmail, IsString, MinLength, IsNotEmpty } from 'class-validator';
+
 export class RegisterDto {
+  @IsEmail()
+  @IsNotEmpty()
   email!: string;
+
+  @IsString()
+  @MinLength(6)
   password!: string;
+
+  @IsString()
+  @IsNotEmpty()
   name!: string;
 }
 
 export class LoginDto {
+  @IsEmail()
+  @IsNotEmpty()
   email!: string;
+
+  @IsString()
+  @IsNotEmpty()
   password!: string;
 }
